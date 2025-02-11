eb2c8ea6530f5351356055950c2484c9abc7b1d7
ho <kimhobig@gmail.com>
2025-02-11 15:00:02 +0900

    initial commit

---
+++ src/main/java/com/BUS/DayFrame/DayFrameApplication.java
@@ -0,0 +1,13 @@
+package com.BUS.DayFrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayFrameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BUS/DayFrame/DayFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BUS.DayFrame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayFrameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
