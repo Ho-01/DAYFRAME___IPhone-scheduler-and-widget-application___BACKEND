826bb1be544f1de33a5d78edeafb8c9e96220031
cherry_pjs <147009498+cherrypjs@users.noreply.github.com>
2025-03-12 18:40:49 +0900

    Update and rename UserRepository.java to UserJpaRepository.java

--- src/main/java/com/BUS/DayFrame/repository/UserJpaRepository.java
+++ src/main/java/com/BUS/DayFrame/repository/UserJpaRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserJpaRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     //이메일이 중복되어있는지 확인용
     boolean existsByEmail(String email);
